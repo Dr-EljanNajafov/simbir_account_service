1989b5a860773959a7651c6b74d0c4e1c01e8c9a
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-29 22:55:53 +0400

    jpa repo enable

--- src/main/java/com/github/simbir_account_service/SimbirAccountServiceApplication.java
+++ src/main/java/com/github/simbir_account_service/SimbirAccountServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients("com.github.simbir_account_service.client")
+@EnableJpaRepositories(basePackages = "com.github.simbir_account_service.repository")
 public class SimbirAccountServiceApplication {
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(SimbirAccountServiceApplication.class)
